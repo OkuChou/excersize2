43e43f61e86d6b7448278bee79cb531f3d86e20b
OkuChou <yizhao2022@yahoo.co.jp>
2025-11-12 13:31:07 +0900

    Initial commit

---
+++ test2.py
@@ -0,0 +1,13 @@
+mylist = ['apple','orange','grape']
+mapped_dict = dict(zip(itr,map(fn,itr)))
+print("hello world")
+
+def func2(): pass
+    
+print("part1")
+print("part2")
+print("part3")
+
+print("パート1")
+print("パート2")
+print("パート3")
---
+++ test3.py
@@ -0,0 +1,7 @@
+print("hello world  3")
+def func1():
+    pass123
+
+change7
+change8
+change9
